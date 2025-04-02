2da5771aacd6e0969ccc3e7215a2d9f1dd850e98
rafih268 <rafi.h2682002@gmail.com>
2025-04-02 01:57:08 +0100

    chore(setup): initial setup for the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# jotter-app
-Notes editor
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+const message = ref('This will be the main page for the Jotter App')
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold underline">{{ message }}</h1>
+</template>
+
+<style scoped>
+</style>
