62a6427dc3352bbda4b40beb99c44d97dba41427
rafih268 <rafi.h2682002@gmail.com>
2025-04-03 23:46:57 +0100

    feat(notes): add implementation for viewing a single note

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -1,1 +1,22 @@
-// This is the component for defining the view of each note in the note list.
+<template>
+  <div class="flex items-center justify-between p-4 border-b border-gray-300">
+    <div>
+      <h3 class="text-lg font-bold">Note Title</h3> <!--This will be replaced with {{ note.title }}-->
+      <p>Note Content</p>
+    </div>
+  </div>
+  <div class="flex items-center">
+    <button
+      @click="$emit(('edit-note', note))"
+      class="mr-2 text-blue-500 cursor-pointer"
+    >Edit</button>
+    <button
+      @click="$emit('delete-note', note.id)"
+      class="text-red-500 cursor-pointer"
+    >Delete</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['note']);
+</script>
