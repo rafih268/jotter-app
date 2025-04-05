6391c8fb14ac2a5367c1709a753aad06472b9931
rafih268 <rafi.h2682002@gmail.com>
2025-04-05 21:11:10 +0100

    chore(app): clean root component from unnecessary imports

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import AddNote from './components/AddNote.vue';
-import EditNote from './components/EditNote.vue';
-import NoteItem from './components/NoteItem.vue';
 import Home from './views/Home.vue';
-const message = ref('This will be the main page for the Jotter App')
 </script>
 
 <template>
