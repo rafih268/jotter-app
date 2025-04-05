202f3f941208f79eaeea7be42cae40f96a9f013c
rafih268 <rafi.h2682002@gmail.com>
2025-04-05 15:08:08 +0100

    feat(home): add scripting for the home view to handle note related functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,43 @@
-// This will be the home page view of this application.
+<template>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+import AddNote from '../components/AddNote.vue';
+import EditNote from '../components/EditNote.vue';
+// import NoteList from '../components/NoteList.vue';
+
+const notes = ref([]);
+
+// Modal visibility for adding and editing notes
+const isAdding = ref(false);
+const isEditing = ref(false);
+
+const currentNote = ref(null);
+
+const addNote = (note) => {
+  notes.value.push(note);
+  isAdding.value = false;
+};
+
+const updateNote = (updatedNote) => {
+  const index = notes.value.findIndex(note => note.id === updatedNote.id);
+  if (index !== -1) {
+    notes.value[index] = updatedNote;
+  }
+
+  isEditing.value = false;
+};
+
+const deleteNote = (noteId) => {
+  notes.value = notes.value.filter(note => note.id !== noteId);
+};
+
+const startEditing = (note) => {
+  currentNote.value = note;
+
+  isEditing.value = true;
+};
+</script>
