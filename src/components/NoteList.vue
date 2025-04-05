ef38d1d5e4694b1231c01ea32cead99b6c6d9edc
rafih268 <rafi.h2682002@gmail.com>
2025-04-05 16:26:11 +0100

    feat(notes): add implementation for viewing list of notes created

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,1 +1,29 @@
-// This is the component for viewing all the existing notes.
+<template>
+  <div class="bg-white p-4 rounded-lg shadow-lg">
+    <template v-if="notes.length > 0">
+      <NoteItem
+        v-for="note in notes"
+        :key="note.id"
+        :task="note"
+        @edit-note="onEdit"
+        @delete-note="onDelete"
+      />
+    </template>
+    <h3 v-else class="text-center text-gray-500">No notes created</h3>
+  </div>
+</template>
+
+<script setup>
+import NoteItem from './NoteItem.vue';
+
+const props = defineProps(['notes']);
+const emit = defineEmits(['edit-note', 'delete-note']);
+
+const onEdit = (note) => {
+  emit('edit-note', note);
+};
+
+const onDelete = (noteId) => {
+  emit('delete-note', noteId);
+};
+</script>
